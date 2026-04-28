c6bdd735b554e01ef533b32642c82289732cdc74
Des Herriott <des.herriott@gmail.com>
2026-04-28 09:20:00 +0100

    chore: remove enderio custom rules
    no longer needed, we have better data in the unifier DB now

--- src/main/java/dev/ftb/mods/ftbmaterials/unification/DefaultCustomRules.java
+++ src/main/java/dev/ftb/mods/ftbmaterials/unification/DefaultCustomRules.java
@@ -32,10 +32,6 @@ public class DefaultCustomRules {
         standardIERule(tweaker, "squeezer");
         standardIERule(tweaker, "sawmill");
 
-        tweaker.addRule(Identifier.fromNamespaceAndPath("enderio", "sag_milling"),
-                new Rule("outputs/item/id", new RewriteAction("id", "ftbmaterials:silicon_gem", "enderio:silicon"))
-        );
-
         try {
             tweaker.save(path);
         } catch (IOException e) {
