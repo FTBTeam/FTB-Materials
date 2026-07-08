3637f49217982267c8b4ef375e091a3822773fd0
Des Herriott <des.herriott@gmail.com>
2026-07-08 07:52:55 +0100

    chore: overrides now works on recipe ID namespace
    (previously on recipe type ID namespace)

--- src/main/java/dev/ftb/mods/ftbmaterials/mixin/RecipeManagerMixin.java
+++ src/main/java/dev/ftb/mods/ftbmaterials/mixin/RecipeManagerMixin.java
@@ -1,8 +1,10 @@
 package dev.ftb.mods.ftbmaterials.mixin;
 
 import com.google.gson.JsonElement;
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.ftb.mods.ftbmaterials.config.StartupConfig;
 import dev.ftb.mods.ftbmaterials.unification.recipe.UnifierManager;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,12 +20,12 @@ public class RecipeManagerMixin {
             ),
             index = 1
     )
-    private Object ftbmaterials$modifyRecipeDecodeArg(Object input) {
+    private Object ftbmaterials$modifyRecipeDecodeArg(Object input, @Local ResourceLocation resourceLocation) {
         if (!StartupConfig.TWEAK_RECIPES.get() || !(input instanceof JsonElement jsonElement)) {
             // input should always be a JSON element but let's be safe...
             return input;
         }
 
-        return UnifierManager.INSTANCE.mutateRecipeJson(jsonElement);
+        return UnifierManager.INSTANCE.mutateRecipeJson(resourceLocation, jsonElement);
     }
 }
